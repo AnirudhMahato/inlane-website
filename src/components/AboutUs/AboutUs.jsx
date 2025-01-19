69f8a61a81796525585b5432a818edb02fcb894a
dheeraj <dheeraj@qblocks.cloud>
2025-01-19 15:54:52 +0530

    Refactor AboutUs component: enhance layout and button styling
    - Updated card styling for improved visual appeal with rounded corners and consistent padding.
    - Adjusted button layout and styling for better alignment and user experience.
    - Enhanced overall design consistency with responsive width adjustments.

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -194,7 +194,7 @@ const AboutUs = () => {
                     ))}
                     {/* PS Message as 6th card */}
                     <div
-                      className="rounded-[24px] p-4 md:p-6"
+                      className="md:rounded-[24px] rounded-3xl p-4 md:p-6 md:h-[58%] w-full"
                       style={{
                         background: "#D1B3FF",
                         position: "relative",
@@ -234,7 +234,9 @@ const AboutUs = () => {
                      href="https://forms.gle/Up128jny4nRz5DH59"
                      target="_blank"
                      rel="noopener noreferrer"
-                     className="inline-flex items-center justify-center ms-3 w-full md:w-auto px-6  py-2  text-lg md:text-xl shadow-xl font-extrabold text-white font-['Bricolage Grotesque'] 
+                     className="inline-flex items-center justify-center ms-3 w-fit
+                      md:w-auto  px-6 
+                       md:py-2  text-lg md:text-xl shadow-xl font-extrabold text-white font-['Bricolage Grotesque'] 
                      rounded-full border-2 border-white bg-gradient-to-r from-[#00CE84] to-[#00BC78] hover:from-[#00CE84] hover:to-[#00CE84] transition-colors duration-200"
                     >
                       <img
