9c450004ee71557f419d15123d1241909cef8584
AnirudhMahato <anirudhmahato2003@gmail.com>
2025-05-31 16:07:53 +0530

    initial commt

--- src/components/FAQ/FAQPage.jsx
+++ src/components/FAQ/FAQPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import faqData from '../../data/faq';
 import Navbar2 from '../Navbar2';
 import Footer from '../Footer';
-import { supabase } from '../../supabaseClient'; // Import Supabase client
+import { supabase } from '../../supabaseClient';
 
 const FAQPage = () => {
   const [activeIndex, setActiveIndex] = useState(null);
@@ -669,7 +669,7 @@ const FAQPage = () => {
                           <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                           </svg>
-                          Raise Query
+                          Raise Quer
                         </>
                       )}
                     </button>
