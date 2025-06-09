d913914f66bc65a3bdcefa27cb99b252acc6d9f6
AnirudhMahato <anirudhmahato2003@gmail.com>
2025-06-09 13:06:32 +0530

    Fix: sync pnpm-lock.yaml with package.json

--- src/app/font.tsx
+++
@@ -1,8 +0,0 @@
-// import { Bricolage_Grotesque } from 'next/font/google';
-
-// export const bricolageGrotesque = Bricolage_Grotesque({
-//   subsets: ['latin'],
-//   weight: ['400', '500', '700'], 
-//   display: 'swap',
-//   variable: '--font-bricolage',
-// });
